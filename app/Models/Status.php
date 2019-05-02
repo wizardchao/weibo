1228778ad738e6e174ae82255d8d4794dc2a20a1
vagrant <vagrant@homestead>
2019-05-02 10:22:15 +0000

    用户微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     public function user()
-{
-    return $this->belongsTo(User::class);
-}
+    {
+        return $this->belongsTo(User::class);
+    }
 }
